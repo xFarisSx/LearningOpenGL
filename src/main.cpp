cf8a2e61e1cff1a88c42afd4f8ec389fe24e48b4
xFarisSx <faristaleek@gmail.com>
2025-08-07 15:49:47 +0300

    Added colors

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,19 @@
 #include "VBO.h"
 #include "shaderClass.h"
 
-GLfloat vertices[] = {-0.5f,     -0.5f * float(sqrt(3.0f)) / 3.0f,       0.0f,
-                      0.5f,      -0.5f * float(sqrt(3.0f)) / 3.0f,       0.0f,
-                      0.0f,      0.5f * float(sqrt(3.0f)) * 2.0f / 3.0f, 0.0f,
+GLfloat vertices[] = {
+    // Positions                               // Colors
+    -0.5f,     -0.5f * float(sqrt(3.0f)) / 3.0f,       0.0f, 0.8f, 0.3f,  0.02f,
+    0.5f,      -0.5f * float(sqrt(3.0f)) / 3.0f,       0.0f, 0.8f, 0.3f,  0.02f,
 
-                      -0.5f / 2, 0.5f * float(sqrt(3.0f)) / 6.0f,        0.0f,
-                      0.5f / 2,  0.5f * float(sqrt(3.0f)) / 6.0f,        0.0f,
-                      0.0f,      -0.5f * float(sqrt(3.0f)) / 3.0f,       0.0f};
+    0.0f,      0.5f * float(sqrt(3.0f)) * 2.0f / 3.0f, 0.0f, 1.0f, 0.6f,  0.32f,
+
+    -0.5f / 2, 0.5f * float(sqrt(3.0f)) / 6.0f,        0.0f, 0.9f, 0.45f, 0.17f,
+
+    0.5f / 2,  0.5f * float(sqrt(3.0f)) / 6.0f,        0.0f, 0.9f, 0.45f, 0.17f,
+
+    0.0f,      -0.5f * float(sqrt(3.0f)) / 3.0f,       0.0f, 0.8f, 0.3f,  0.02f,
+};
 
 GLuint indices[] = {0, 3, 5, 3, 2, 4, 5, 4, 1};
 
@@ -38,30 +44,39 @@ int main() {
     glfwTerminate();
     return -1;
   }
+
   // Introduce the window into the current context
   glfwMakeContextCurrent(window);
 
   // Load GLAD so it configures OpenGL
   gladLoadGL();
 
+  int width, height;
+  glfwGetFramebufferSize(window, &width, &height);
+  glViewport(0, 0, width, height);
+
   // Specify the viewport of OpenGL in the Window
-  glViewport(0, 0, 800, 800);
+  glViewport(0, 0, width, height);
 
-  Shader shaderProgram("../shaders/vertexShader.vert", "../shaders/fragmentShader.frag");
+  Shader shaderProgram("../shaders/vertexShader.vert",
+                       "../shaders/fragmentShader.frag");
 
-  VAO VAO1; 
+  VAO VAO1;
   VAO1.Bind();
 
   VBO VBO1(vertices, sizeof(vertices));
   EBO EBO1(indices, sizeof(indices));
 
-  VAO1.LinkVBO(VBO1,0);
+  VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void *)0);
+  VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float),
+                  (void *)(3 * sizeof(float)));
+
   VAO1.Unbind();
   VBO1.Unbind();
   EBO1.Unbind();
 
+  GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
-  
   // Specify color of background
   glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
   // Clean the back buffer
@@ -76,20 +91,20 @@ int main() {
 
     glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
-    
+
     shaderProgram.Activate();
+    glUniform1f(uniID, 0.f);
 
     VAO1.Bind();
     glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 
     glfwSwapBuffers(window);
   }
-  
+
   VAO1.Delete();
   VBO1.Delete();
   EBO1.Delete();
   shaderProgram.Delete();
-
 
   glfwDestroyWindow(window);
   glfwTerminate();
